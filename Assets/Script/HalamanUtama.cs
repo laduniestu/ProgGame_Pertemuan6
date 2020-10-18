355b21d8a2eae59e312d41bbabbf09e6c6197d58
laduniestu <duniedun@gmail.com>
2020-10-19 02:35:29 +0700

    Tugas selesai

---
+++ Assets/Script/HalamanUtama.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanUtama : MonoBehaviour
+{
+    public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+    public void KeluarAplikasi()
+    {
+        Application.Quit();
+    }
+}
